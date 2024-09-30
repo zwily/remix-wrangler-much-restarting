f15a8dfb470bcbcefe06483d6342d4e64a50e1e3
Zach Wily <zach@zwily.com>
2024-09-30 13:42:33 -0600

    update to use workers static assets

--- server.ts
+++ server.ts
@@ -1,11 +1,6 @@
-import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
 import type { AppLoadContext } from "@remix-run/cloudflare";
 import { createRequestHandler, logDevReady } from "@remix-run/cloudflare";
 import * as build from "@remix-run/dev/server-build";
-// eslint-disable-next-line import/no-unresolved
-import __STATIC_CONTENT_MANIFEST from "__STATIC_CONTENT_MANIFEST";
-
-const MANIFEST = JSON.parse(__STATIC_CONTENT_MANIFEST);
 const handleRemixRequest = createRequestHandler(build, process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === "development") {
@@ -17,32 +12,8 @@ export default {
     request: Request,
     env: {
       __STATIC_CONTENT: Fetcher;
-    },
-    ctx: ExecutionContext
-  ): Promise<Response> {
-    try {
-      const url = new URL(request.url);
-      const ttl = url.pathname.startsWith("/build/")
-        ? 60 * 60 * 24 * 365 // 1 year
-        : 60 * 5; // 5 minutes
-      return await getAssetFromKV(
-        {
-          request,
-          waitUntil: ctx.waitUntil.bind(ctx),
-        } as FetchEvent,
-        {
-          ASSET_NAMESPACE: env.__STATIC_CONTENT,
-          ASSET_MANIFEST: MANIFEST,
-          cacheControl: {
-            browserTTL: ttl,
-            edgeTTL: ttl,
-          },
-        }
-      );
-    } catch (error) {
-      // No-op
     }
-
+  ): Promise<Response> {
     try {
       const loadContext: AppLoadContext = {
         env,
