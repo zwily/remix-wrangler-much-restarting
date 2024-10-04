6727f5d1d3aa76ec02880d49b9c3ef39528135f2
Zach Wily <zach@zwily.com>
2024-10-04 10:02:34 -0600

    update for latest proposed remix classic compiler cloudflare workers template

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
-import type { AppLoadContext } from "@remix-run/cloudflare";
 import { createRequestHandler, logDevReady } from "@remix-run/cloudflare";
 import * as build from "@remix-run/dev/server-build";
+import { getLoadContext } from "./load-context";
+
 const handleRemixRequest = createRequestHandler(build, process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === "development") {
@@ -8,20 +9,30 @@ if (process.env.NODE_ENV === "development") {
 }
 
 export default {
-  async fetch(
-    request: Request,
-    env: {
-      __STATIC_CONTENT: Fetcher;
-    }
-  ): Promise<Response> {
+  async fetch(request, env, ctx): Promise<Response> {
     try {
-      const loadContext: AppLoadContext = {
-        env,
-      };
+      const loadContext = getLoadContext({
+        request,
+        context: {
+          cloudflare: {
+            // This object matches the return value from Wrangler's
+            // `getPlatformProxy` used during development via Remix's
+            // `cloudflareDevProxyVitePlugin`:
+            // https://developers.cloudflare.com/workers/wrangler/api/#getplatformproxy
+            cf: request.cf,
+            ctx: {
+              waitUntil: ctx.waitUntil.bind(ctx),
+              passThroughOnException: ctx.passThroughOnException.bind(ctx),
+            },
+            caches,
+            env,
+          },
+        },
+      });
       return await handleRemixRequest(request, loadContext);
     } catch (error) {
       console.log(error);
       return new Response("An unexpected error occurred", { status: 500 });
     }
   },
-};
+} satisfies ExportedHandler<Env>;
